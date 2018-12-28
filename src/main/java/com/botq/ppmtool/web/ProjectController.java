6ed4716cb687ea24c7a2bb7b83abf8cb132b4454
Patryk Szwed <patryk.szwed@outlook.com>
2018-12-28 16:43:42 +0100

    Added ability to retrieve all projects at once

--- src/main/java/com/botq/ppmtool/web/ProjectController.java
+++ src/main/java/com/botq/ppmtool/web/ProjectController.java
@@ -46,4 +46,9 @@ public class ProjectController {
 
         return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects(){
+        return projectService.findAllProjects();
+    }
 }
