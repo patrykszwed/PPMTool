85739c471dc266b869099395e839313953e29997
Patryk Szwed <patryk.szwed@outlook.com>
2019-01-03 17:48:40 +0100

    Dashboard now retrieves all projects

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
 import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
+  // this will call getProjects() while the component is mounted
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
+    // this is the same as const projects = this.props.project.projects
+    const { projects } = this.props.project;
+
     return (
       <div className="projects">
         <div className="container">
@@ -14,7 +25,9 @@ class Dashboard extends Component {
               <CreateProjectButton />
               <br />
               <hr />
-              <ProjectItem />
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
@@ -23,4 +36,16 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project
+});
+
+export default connect(
+  mapStateToProps,
+  { getProjects }
+)(Dashboard);
