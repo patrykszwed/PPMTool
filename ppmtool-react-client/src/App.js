36d792c3c92a6f2d6fb8f934579fae35cf18ef96
Patryk Szwed <patryk.szwed@outlook.com>
2019-01-03 04:23:41 +0100

    Wired up backend with frontend while creating new project

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,14 +3,23 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
