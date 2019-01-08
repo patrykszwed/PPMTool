9a3e2f491c7d8a9d9d7a7c139a2e1445c859ca07
Patryk Szwed <patryk.szwed@outlook.com>
2019-01-08 11:47:05 +0100

    Added Controller and Service for Backlog and ProjectTask

---
+++ PPMToolFullStack/src/main/java/com/botq/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,47 @@
+package com.botq.ppmtool.services;
+
+import com.botq.ppmtool.domain.Backlog;
+import com.botq.ppmtool.domain.ProjectTask;
+import com.botq.ppmtool.repositories.BacklogRepository;
+import com.botq.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+
+        // Project Tasks to be added to a specific project, project != null => Backlog exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        // set the Backlog to Project Task
+        projectTask.setBacklog(backlog);
+        // we want our project sequence to be for example like this: IDPRO-1, IDPRO-2, etc.
+        Integer backlogSequence = backlog.getPTSequence();
+        // Update the Backlog Sequence
+        backlogSequence++;
+
+        backlog.setPTSequence(backlogSequence);
+
+        //Add Sequence to Project Task
+        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        // INITIAL priority when priority == null
+        if(projectTask.getPriority() == null){ // In the future we need projectTask.getPriority() == 0 to handle the form
+            projectTask.setPriority(3);
+        }
+        // INITIAL status when status == null
+        if(projectTask.getStatus() == null || projectTask.getStatus().equals("")){
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
